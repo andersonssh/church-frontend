ff377ad38988a1a0e87fa9f5f3e67f9c952f901a
andersonssh <4nd3r50n4r4uj0@gmail.com>
2022-01-31 23:30:16 -0300

    Adiciona projeto inicial

---
+++ igreja/src/App.js
@@ -0,0 +1,14 @@
+function App(){
+  return (
+    <div>
+      <button>seila</button>
+      <a href="#">Link aqui</a>
+      <h1>h1</h1>
+      <h2>h2</h2>
+      <h3>h3</h3>
+      <h4>h4</h4>
+    </div>
+  )
+}
+
+export default App
