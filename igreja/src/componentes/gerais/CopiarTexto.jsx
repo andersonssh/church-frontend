33ccd81dfdaa557e2451a09a0d80b7bd196f6ea5
andersonssh <4nd3r50n4r4uj0@gmail.com>
2022-02-02 10:47:10 -0300

    Cria modulo de copiar textos

---
+++ igreja/src/componentes/gerais/CopiarTexto.jsx
@@ -0,0 +1,26 @@
+import style from './CopiarTexto.module.css'
+import { useState } from 'react'
+
+
+function CopiarTexto({texto}){
+    const [icone, setIcone] = useState('fas fa-copy')
+    function acao(jose){
+        console.log(this)
+        copiarTexto()
+        trocarIcone()
+    }
+    function trocarIcone(){
+        setIcone(`fas fa-check ${style.ativo}`)
+        setTimeout(() => {
+            setIcone(`fas fa-copy `)
+        }, 3000);
+    }
+    function copiarTexto() {
+        navigator.clipboard.writeText(texto)
+    }   
+    return (
+        <button className={style.botao} onClick={acao}><i className={icone}></i></button>
+    )
+}
+
+export default CopiarTexto
