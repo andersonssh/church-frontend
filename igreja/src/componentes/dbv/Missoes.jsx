22282c40124a70332b5db638d2f32f337c9680d9
andersonssh <4nd3r50n4r4uj0@gmail.com>
2022-02-07 14:40:49 -0300

    Adiciona caixa de informacao a cada bloco

--- igreja/src/componentes/dbv/Missoes.jsx
+++ igreja/src/componentes/dbv/Missoes.jsx
@@ -1,23 +1,91 @@
 import style from './Missoes.module.css'
+import { useState } from 'react'
+
 
 function Missoes(){
+    function abrirModal(tipo, indice){
+        if (tipo === 'especial'){
+            setTituloModal(dados.especiais[indice][0])
+            setInfoModal(dados.especiais[indice][3])
+        }else if (tipo === 'comum'){
+            setTituloModal(dados.missoes[indice][0])
+            setInfoModal(dados.missoes[indice][2])
+        }
+    }
+
+    const [tituloModal, setTituloModal] = useState()
+    const [infoModal, setInfoModal] = useState()
+
+    // CRIAR ELEMENTOS 
     let dados = {
         especiais: [
-            ['Missão mensal (surpresa)', '300-700'],
+            ['Criar o grito de guerra do clube', '350'],
         ],
         missoes: [
-            ['Completar a classe', 1500],
-            ['Tornar o amigo um desbravador', 500],
-            ['Finalizar um estudo bíblico', 400],
-            ['Concluir uma nova especialidade', 200],
-            ['Mensalidade em dia', 150],
-            ['Participar ativamente dos eventos da igreja', 100],
-            ['Frequência', 50],
-            ['Levar um amigo à reunião', 50],
-            ['Uniforme', 30],
-            ['Pontualidade', 30],
-            ['Levar Material COMPLETO', 30],
-            ['Apresentação nós', 30]
+            ['Finalizar um estudo bíblico', 400,(
+                    <div>
+                        Em dupla, dar um estudo bíblico completo para alguém.
+                    </div>
+                )],
+            ['Concluir uma nova especialidade', 200, (
+                <div>
+                    Finalizar uma especialidade sozinho ou em grupo.
+                </div>
+            )],
+            ['Mensalidade em dia', 150, (
+                <div>
+                    Pagar a mensalidade até o último dia do mês.
+                </div>
+            )],
+            ['Levar um amigo à reunião', 50, (
+                <div>Levar um amigo a Reunião dos desbravadores. A pontuação é dada uma vez para cada nova pessoa.</div>
+            )],
+            ['Participar ativamente dos eventos da igreja', 100, (
+                <div>
+                    Participar dos eventos promovidos pela igreja. Aqueles que ajudarem e se dedicarem visívelmente além do esperado, poderão atingir mais pontos.
+                </div>
+            )],
+            ['Frequência', 50, (
+                <div>
+                    Marcar presença em cada reunião que tenha relação com o clube.
+                </div>
+            )],
+            ['Uniforme', 30, (
+                <div>
+                    Ir para as reuniões com o fardamento completo.
+                </div>
+            )],
+            ['Pontualidade', 30, (
+                <div>
+                    Chegar nas reuniões no horário correto.
+                </div>
+            )],
+            ['Levar Material COMPLETO', 30, (
+                <div>
+                    Levar todo o material solicitado para o dia, incluindo materiais que forem solicitados para alguma dinâmica.
+                </div>
+            )],
+            ['Apresentação nós', 30, (
+                <div>
+                    Em uma única tentativa, apresentar o nó corretamente.
+                </div>
+            )],
+            ['Dinâmicas', '10-50', (
+                <div>
+                    Pontuação para cada dinâmica individual:
+                    <div>1º lugar: 60</div>
+                    <div>2º lugar: 40</div>
+                    <div>3º lugar: 30</div>
+                    <div>4º lugar: 20</div>
+                    <div>5º lugar: 10</div>
+                    <div>6º lugar +: 10</div>
+                    Pontuação para cada dinâmica em grupo:
+                    <div>1º lugar: 60</div>
+                    <div>2º lugar: 40</div>
+                    <div>3º lugar: 20</div>
+                    <div>4º lugar + : 20</div>
+                </div>
+            )]
         ]
     }
         
@@ -25,7 +93,8 @@ function Missoes(){
     // adicionando primeiro as missoes especiais
     for (let i = 0; i < dados.especiais.length; i++){
         listaMissoes.push(
-            <div className={'col-sm-6 col-md-4 col-lg-3 ' + style.especiais}>
+            <div onClick={ () => abrirModal('especial', i) }className={'col-sm-6 col-md-4 col-lg-3 ' + style.especiais}>
+                <button className={style.botao_info}><i class="fas fa-info-circle" data-bs-toggle="modal" data-bs-target="#infoMissao"></i></button>
                 <div>{dados.especiais[i][0]}</div>
                 <div className={style.pontuacao}>{dados.especiais[i][1]} pontos</div>
             </div>
@@ -36,6 +105,7 @@ function Missoes(){
     for (let i = 0; i < dados.missoes.length; i++){
         listaMissoes.push(
             <div className={'col-sm-6 col-md-4 col-lg-3 ' + style.missoes}>
+                <button onClick={ () => abrirModal('comum', i) } className={style.botao_info} data-bs-toggle="modal" data-bs-target="#infoMissao"><i class="fas fa-info-circle"></i></button>
                 <div>{dados.missoes[i][0]}</div>
                 <div className={style.pontuacao}>{dados.missoes[i][1]} pontos</div>
             </div>
@@ -55,6 +125,22 @@ function Missoes(){
             </div>
             <div className="row justify-content-center mb-5">
                 {listaMissoes}
+            </div>
+            <div class="modal fade" id="infoMissao" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">{tituloModal}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    {infoModal}
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-success" data-bs-dismiss="modal">Entendido!</button>
+                </div>
+                </div>
+            </div>
             </div>
         </div>
     )
